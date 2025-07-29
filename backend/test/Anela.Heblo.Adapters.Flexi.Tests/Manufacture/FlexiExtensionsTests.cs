4bd07b7cd769f726fe6535358060e17cc8e4d257
ondrej.pajgrt <ondrej.pajgrt@notino.com>
2025-07-29 21:26:05 +0200

    feat: flexi manufacture unit tests

---
+++ backend/test/Anela.Heblo.Adapters.Flexi.Tests/Manufacture/FlexiExtensionsTests.cs
@@ -0,0 +1,28 @@
+using Anela.Heblo.Adapters.Flexi.Manufacture;
+using FluentAssertions;
+using Xunit;
+
+namespace Anela.Heblo.Adapters.Flexi.Tests.Manufacture;
+
+public class FlexiExtensionsTests
+{
+    [Theory]
+    [InlineData("code:PRODUCT123", "PRODUCT123")]
+    [InlineData("code:  PRODUCT123  ", "PRODUCT123")]
+    [InlineData("PRODUCT123", "PRODUCT123")]
+    [InlineData("code:", "")]
+    [InlineData("", "")]
+    [InlineData(null, null)]
+    [InlineData("  code:PRODUCT123  ", "PRODUCT123")]
+    [InlineData("code:code:PRODUCT123", "code:PRODUCT123")]
+    [InlineData("CODE:PRODUCT123", "CODE:PRODUCT123")]
+    [InlineData("prefix:code:PRODUCT123", "prefix:code:PRODUCT123")]
+    public void RemoveCodePrefix_ReturnsExpectedResult(string input, string expected)
+    {
+        // Act
+        var result = input.RemoveCodePrefix();
+        
+        // Assert
+        result.Should().Be(expected);
+    }
+}
---
+++ backend/test/Anela.Heblo.Adapters.Shoptet.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace Anela.Heblo.Adapters.Shoptet.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
