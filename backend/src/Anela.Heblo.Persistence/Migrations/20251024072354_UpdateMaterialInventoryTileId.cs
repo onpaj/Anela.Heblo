8b31d0066c82ea5f3cbfc5147ba8619553cb4540
pajgrtondrej <pajgrt.ondrej@gmail.com>
2025-10-24 09:26:44 +0200

    feat: material with and without expiration tile

---
+++ backend/src/Anela.Heblo.Persistence/Migrations/20251024072354_UpdateMaterialInventoryTileId.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Anela.Heblo.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateMaterialInventoryTileId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                -- First delete any existing materialwithexpirationinventorysummary tiles to avoid constraint conflicts
+                DELETE FROM ""UserDashboardTiles""
+                WHERE ""TileId"" = 'materialwithexpirationinventorysummary';
+                
+                -- Then update materialinventorysummary to materialwithexpirationinventorysummary
+                UPDATE ""UserDashboardTiles""
+                SET ""TileId"" = 'materialwithexpirationinventorysummary'
+                WHERE ""TileId"" = 'materialinventorysummary';
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE ""UserDashboardTiles""
+                SET ""TileId"" = 'materialinventorysummary'
+                WHERE ""TileId"" = 'materialwithexpirationinventorysummary'
+            ");
+        }
+    }
+}
