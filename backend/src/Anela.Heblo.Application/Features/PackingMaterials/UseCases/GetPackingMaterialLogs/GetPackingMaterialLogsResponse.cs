c5ea0d0c9bcb9c990915f4f7d0638bd0d88ea3dc
pajgrtondrej <pajgrt.ondrej@gmail.com>
2025-11-20 20:41:50 +0100

    fix: be tests

--- backend/src/Anela.Heblo.Application/Features/PackingMaterials/UseCases/GetPackingMaterialLogs/GetPackingMaterialLogsResponse.cs
+++ backend/src/Anela.Heblo.Application/Features/PackingMaterials/UseCases/GetPackingMaterialLogs/GetPackingMaterialLogsResponse.cs
@@ -1,8 +1,9 @@
 using Anela.Heblo.Application.Features.PackingMaterials.Contracts;
+using Anela.Heblo.Application.Shared;
 
 namespace Anela.Heblo.Application.Features.PackingMaterials.UseCases.GetPackingMaterialLogs;
 
-public class GetPackingMaterialLogsResponse
+public class GetPackingMaterialLogsResponse : BaseResponse
 {
     public PackingMaterialDto Material { get; set; } = null!;
     public IEnumerable<PackingMaterialLogDto> Logs { get; set; } = new List<PackingMaterialLogDto>();
