86e0af585da7623b91c0181141ab4e2f10a08155
pajgrtondrej <pajgrt.ondrej@gmail.com>
2026-03-26 14:42:08 +0100

    fix: allow to rerun expedition list job

---
+++ backend/src/Anela.Heblo.Application/Features/ExpeditionList/UseCases/RunExpeditionListPrintFix/RunExpeditionListPrintFixResponse.cs
@@ -0,0 +1,8 @@
+namespace Anela.Heblo.Application.Features.ExpeditionList.UseCases.RunExpeditionListPrintFix;
+
+public class RunExpeditionListPrintFixResponse
+{
+    public bool Success { get; set; }
+    public int TotalCount { get; set; }
+    public string? ErrorMessage { get; set; }
+}
