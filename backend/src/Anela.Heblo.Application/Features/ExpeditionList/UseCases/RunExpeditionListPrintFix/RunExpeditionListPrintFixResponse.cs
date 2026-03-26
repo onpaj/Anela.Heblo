6a7d6cb7113c2921890e25b11f66e8b87a61826d
pajgrtondrej <pajgrt.ondrej@gmail.com>
2026-03-26 14:55:50 +0100

    fix: base response class

--- backend/src/Anela.Heblo.Application/Features/ExpeditionList/UseCases/RunExpeditionListPrintFix/RunExpeditionListPrintFixResponse.cs
+++ backend/src/Anela.Heblo.Application/Features/ExpeditionList/UseCases/RunExpeditionListPrintFix/RunExpeditionListPrintFixResponse.cs
@@ -1,8 +1,9 @@
+using Anela.Heblo.Application.Shared;
+
 namespace Anela.Heblo.Application.Features.ExpeditionList.UseCases.RunExpeditionListPrintFix;
 
-public class RunExpeditionListPrintFixResponse
+public class RunExpeditionListPrintFixResponse : BaseResponse
 {
-    public bool Success { get; set; }
     public int TotalCount { get; set; }
     public string? ErrorMessage { get; set; }
 }
