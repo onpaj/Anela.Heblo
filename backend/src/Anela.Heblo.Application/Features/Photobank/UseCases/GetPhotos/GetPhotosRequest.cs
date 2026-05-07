c517df65582beb2fb8ace1536af5d9f63098ce5e
pajgrtondrej <pajgrt.ondrej@gmail.com>
2026-05-07 17:29:16 +0200

    feat(photobank): add UseFolderRegex property to GetPhotosRequest

--- backend/src/Anela.Heblo.Application/Features/Photobank/UseCases/GetPhotos/GetPhotosRequest.cs
+++ backend/src/Anela.Heblo.Application/Features/Photobank/UseCases/GetPhotos/GetPhotosRequest.cs
@@ -9,6 +9,7 @@ namespace Anela.Heblo.Application.Features.Photobank.UseCases.GetPhotos
         public string? Search { get; set; }
         public bool UseRegex { get; set; }
         public string? FolderPath { get; set; }
+        public bool UseFolderRegex { get; set; }
         public bool WithoutTags { get; set; }
         public int Page { get; set; } = 1;
         public int PageSize { get; set; } = 48;
