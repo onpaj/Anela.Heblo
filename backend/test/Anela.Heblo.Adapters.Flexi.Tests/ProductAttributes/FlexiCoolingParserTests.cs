9179c77eb3adf5dbcae29e691a6a026d125b5194
pajgrtondrej <pajgrt.ondrej@gmail.com>
2026-05-18 11:30:24 +0200

    feat: add Cooling property to CatalogAttributes

---
+++ backend/test/Anela.Heblo.Adapters.Flexi.Tests/ProductAttributes/FlexiCoolingParserTests.cs
@@ -0,0 +1,15 @@
+using Anela.Heblo.Domain.Features.Catalog;
+using Anela.Heblo.Domain.Features.Catalog.Attributes;
+using FluentAssertions;
+
+namespace Anela.Heblo.Adapters.Flexi.Tests.ProductAttributes;
+
+public class FlexiCoolingParserTests
+{
+    [Fact]
+    public void CatalogAttributes_HasCoolingProperty_DefaultsToNone()
+    {
+        var attrs = new CatalogAttributes();
+        attrs.Cooling.Should().Be(Cooling.None);
+    }
+}
