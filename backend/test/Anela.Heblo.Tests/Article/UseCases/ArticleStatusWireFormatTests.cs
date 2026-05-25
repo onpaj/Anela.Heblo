307170803ec0627c05a523d3dd6c5fc2b7e6ce55
pajgrtondrej <pajgrt.ondrej@gmail.com>
2026-05-25 20:42:55 +0200

    test: lock JSON wire format for ArticleStatus on response DTOs

---
+++ backend/test/Anela.Heblo.Tests/Article/UseCases/ArticleStatusWireFormatTests.cs
@@ -0,0 +1,40 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Anela.Heblo.Application.Features.Article.UseCases.GetArticle;
+using Anela.Heblo.Application.Features.Article.UseCases.ListArticles;
+using Anela.Heblo.Domain.Features.Article;
+using FluentAssertions;
+
+namespace Anela.Heblo.Tests.Article.UseCases;
+
+public class ArticleStatusWireFormatTests
+{
+    private static readonly JsonSerializerOptions ApiJsonOptions = new()
+    {
+        Converters = { new JsonStringEnumConverter() },
+    };
+
+    [Theory]
+    [InlineData(ArticleStatus.Generated, "\"Status\":\"Generated\"")]
+    [InlineData(ArticleStatus.Queued, "\"Status\":\"Queued\"")]
+    public void GetArticleResponse_SerializesStatusAsEnumName(ArticleStatus status, string expectedFragment)
+    {
+        var response = new GetArticleResponse { Status = status };
+
+        var json = JsonSerializer.Serialize(response, ApiJsonOptions);
+
+        json.Should().Contain(expectedFragment);
+    }
+
+    [Theory]
+    [InlineData(ArticleStatus.Generated, "\"Status\":\"Generated\"")]
+    [InlineData(ArticleStatus.Queued, "\"Status\":\"Queued\"")]
+    public void ArticleListItemDto_SerializesStatusAsEnumName(ArticleStatus status, string expectedFragment)
+    {
+        var item = new ArticleListItemDto { Status = status };
+
+        var json = JsonSerializer.Serialize(item, ApiJsonOptions);
+
+        json.Should().Contain(expectedFragment);
+    }
+}
