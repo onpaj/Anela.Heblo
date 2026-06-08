d7fdea6c723faf267f229f4a87a5bfd15a26daa1
pajgrtondrej <pajgrt.ondrej@gmail.com>
2026-06-08 07:32:46 +0200

    style: use file-scoped namespace in ArticleTests

--- backend/test/Anela.Heblo.Tests/Domain/Article/ArticleTests.cs
+++ backend/test/Anela.Heblo.Tests/Domain/Article/ArticleTests.cs
@@ -2,45 +2,44 @@ using Anela.Heblo.Domain.Features.Article;
 using FluentAssertions;
 using Xunit;
 
-namespace Anela.Heblo.Tests.Domain.Article
+namespace Anela.Heblo.Tests.Domain.Article;
+
+public class ArticleTests
 {
-    public class ArticleTests
+    [Fact]
+    public void SubmitFeedback_AssignsAllThreeProperties()
     {
-        [Fact]
-        public void SubmitFeedback_AssignsAllThreeProperties()
+        var article = new Heblo.Domain.Features.Article.Article
         {
-            var article = new Heblo.Domain.Features.Article.Article
-            {
-                Id = Guid.NewGuid(),
-                Topic = "topic",
-            };
+            Id = Guid.NewGuid(),
+            Topic = "topic",
+        };
 
-            article.PrecisionScore.Should().BeNull();
-            article.StyleScore.Should().BeNull();
-            article.FeedbackComment.Should().BeNull();
+        article.PrecisionScore.Should().BeNull();
+        article.StyleScore.Should().BeNull();
+        article.FeedbackComment.Should().BeNull();
 
-            article.SubmitFeedback(precisionScore: 5, styleScore: 4, comment: "Great");
+        article.SubmitFeedback(precisionScore: 5, styleScore: 4, comment: "Great");
 
-            article.PrecisionScore.Should().Be(5);
-            article.StyleScore.Should().Be(4);
-            article.FeedbackComment.Should().Be("Great");
-        }
+        article.PrecisionScore.Should().Be(5);
+        article.StyleScore.Should().Be(4);
+        article.FeedbackComment.Should().Be("Great");
+    }
 
-        [Fact]
-        public void SubmitFeedback_NullComment_IsAllowed()
+    [Fact]
+    public void SubmitFeedback_NullComment_IsAllowed()
+    {
+        var article = new Heblo.Domain.Features.Article.Article
         {
-            var article = new Heblo.Domain.Features.Article.Article
-            {
-                Id = Guid.NewGuid(),
-                Topic = "topic",
-            };
+            Id = Guid.NewGuid(),
+            Topic = "topic",
+        };
 
-            var act = () => article.SubmitFeedback(precisionScore: 3, styleScore: 3, comment: null);
+        var act = () => article.SubmitFeedback(precisionScore: 3, styleScore: 3, comment: null);
 
-            act.Should().NotThrow();
-            article.PrecisionScore.Should().Be(3);
-            article.StyleScore.Should().Be(3);
-            article.FeedbackComment.Should().BeNull();
-        }
+        act.Should().NotThrow();
+        article.PrecisionScore.Should().Be(3);
+        article.StyleScore.Should().Be(3);
+        article.FeedbackComment.Should().BeNull();
     }
 }
